5e26b7c45f14017b7bd1a696286f47433d556867
jvan <jvan@jvanlabs.net>
2013-04-25 13:53:40 -0700

    Replaced CMake with distutils for build system.

--- setup.py
+++ setup.py
@@ -8,20 +8,20 @@ from distutils.core import setup, Extension
 import subprocess
 
 def pkg_config(*args):
-   return subprocess.check_output(('pkg-config',)+args).decode('utf-8').strip()
+   return subprocess.check_output(('pkg-config',)+args).decode('utf-8').split()
 
-def unflag(argstr):
-   return [x[2:] for x in argstr.split()]
+def unflag(args):
+   return [x[2:] for x in args]
 
 pyvrui_module = Extension(
    '_pyvrui',
    ['pyvrui.i'],
-   swig_opts = ['-c++', pkg_config('--cflags-only-I', 'Vrui')],
+   swig_opts = ['-c++'] + pkg_config('--cflags-only-I', 'Vrui'),
    include_dirs = ['./src'] + unflag(pkg_config('--cflags-only-I', 'Vrui')),
-   extra_compile_args = [pkg_config('--cflags-only-other', 'Vrui')],
+   extra_compile_args = pkg_config('--cflags-only-other', 'Vrui'),
    library_dirs = unflag(pkg_config('--libs-only-L', 'Vrui')),
    libraries = unflag(pkg_config('--libs-only-l', 'Vrui')),
-   extra_link_args = [pkg_config('--libs-only-other', 'Vrui')]
+   extra_link_args = pkg_config('--libs-only-other', 'Vrui')
    )
 
 setup(
